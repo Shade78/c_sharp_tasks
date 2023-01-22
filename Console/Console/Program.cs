eeade26f1f5ed86f0b03f2366a3110a076f92989
Shade78 <43784874+Shade78@users.noreply.github.com>
2023-01-22 17:12:29 +0300

    Add files via upload

---
+++ Console/Console/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main()
+    {
+
+        Console.WriteLine("Введите значение a");
+        int a = Int32.Parse(Console.ReadLine());
+
+        var z1 = (Math.Sin(2 * a) + Math.Sin(5 * a) - Math.Sin(3 * a)) / (Math.Cos(a) + 1 - 2 * Math.Pow(Math.Sin(2 * a), 2));
+        var z2 = 2 * Math.Sin(a);
+       
+        double v = Math.Round(z1, 3);
+        double c = Math.Round(z2, 3);
+
+        Console.WriteLine(v + " " + c);
+    }
+}
