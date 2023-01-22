ceae09f1b3338c7af2adbf914eafc9e41712b517
Shade78 <43784874+Shade78@users.noreply.github.com>
2023-01-22 17:31:00 +0300

    Add files via upload
    Составить программу, которая принимает с клавиатуры строку произвольной длины и выводит на экран преобразованную строку, в которой предварительно удалены все цифры.

---
+++ 4th_task/4th_task/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _4th_task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку произвольной длины");
+            string str = Console.ReadLine();
+            string str2 = "";
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] < '0' || str[i] > '9')
+                {
+                    str2 += str[i];
+                }
+            }
+            Console.WriteLine(str2);
+            Console.ReadKey();
+        }
+    }
+}
